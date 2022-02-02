3b6147676df61a7bc55ea1f2352bbfb689f1a6c0
sik2 <devpang20@gmail.com>
2022-02-02 18:35:46 +0900

    feat: 데이터 연동

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="black-bg" v-if="isOpen == true">
       <div class="white-bg">
-        <h4>상세페이지</h4>
+        <h4>{{ oneRooms[clickIndex].title }}</h4>
         <p>상세페이지 내용임</p>
         <button @click="isOpen = false">닫기</button>
       </div>
@@ -10,35 +10,25 @@
     <div class="menu">
       <a v-for="(a, i) in menus" :key="i" >{{ a }}</a>
     </div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div>
-      <img src="./assets/room0.jpg" alt="썸넬" class="room-img">
-      <h4 @click="isOpen = true">{{ products[0] }} 원룸</h4>
-      <p>가격은 아무거나~</p>
-      <button @click="count[0]++">허위매물</button> <span>신고수 : {{ count[0] }}</span>
-    </div>
-    <div>
-      <img src="./assets/room1.jpg" alt="썸넬" class="room-img">
-      <h4 @click="isOpen = true">{{ products[1] }} 원룸</h4>
-      <p>가격은 아무거나~</p>
-      <button @click="count[1]++">허위매물</button> <span>신고수 : {{ count[1] }}</span>
-    </div>
-    <div>
-      <img src="./assets/room2.jpg" alt="썸넬" class="room-img">
-      <h4 @click="isOpen = true">{{ products[2] }} 원룸</h4>
-      <p>가격은 아무거나~</p>
-      <button @click="count[2]++">허위매물</button> <span>신고수 : {{ count[2] }}</span>
+    <div v-for="(room, i) in oneRooms" :key="i">
+      <img :src="room.image" alt="썸넬" class="room-img">
+      <h4 @click="isOpen = true; clickIndex = i">{{ room.title }} 원룸</h4>
+      <p>{{ room.price }}</p>
     </div>
   </div>
 </template>
 
 <script>
 
+import data from './assets/oneroom'
+
 export default {
   name: 'App',
   //react 의 state
   data() {
     return {
+      oneRooms :data,
+      clickIndex : 0,
       isOpen: false,
       count: [0, 0, 0],
       menus: ['Home', 'Shop', 'About'],
