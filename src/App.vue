5fb091cf038649bbab9e0d4228ad54eeb1b2ccb7
sik2 <devpang20@gmail.com>
2022-02-02 17:54:49 +0900

    feat:매물페이지, 신고 기능, 모달 추가

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
+    <div class="black-bg" v-if="isOpen == true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지 내용임</p>
+        <button @click="isOpen = false">닫기</button>
+      </div>
+    </div>
     <div class="menu">
-      <a>Home</a>
-      <a>Products</a>
-      <a>About</a>
+      <a v-for="(a, i) in menus" :key="i" >{{ a }}</a>
     </div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <div v-for="(product, i) in products" :key="i">
-      <h4>{{ product }} 원룸</h4>
+    <div>
+      <img src="./assets/room0.jpg" alt="썸넬" class="room-img">
+      <h4 @click="isOpen = true">{{ products[0] }} 원룸</h4>
       <p>가격은 아무거나~</p>
+      <button @click="count[0]++">허위매물</button> <span>신고수 : {{ count[0] }}</span>
+    </div>
+    <div>
+      <img src="./assets/room1.jpg" alt="썸넬" class="room-img">
+      <h4 @click="isOpen = true">{{ products[1] }} 원룸</h4>
+      <p>가격은 아무거나~</p>
+      <button @click="count[1]++">허위매물</button> <span>신고수 : {{ count[1] }}</span>
+    </div>
+    <div>
+      <img src="./assets/room2.jpg" alt="썸넬" class="room-img">
+      <h4 @click="isOpen = true">{{ products[2] }} 원룸</h4>
+      <p>가격은 아무거나~</p>
+      <button @click="count[2]++">허위매물</button> <span>신고수 : {{ count[2] }}</span>
     </div>
   </div>
 </template>
@@ -17,9 +36,18 @@
 
 export default {
   name: 'App',
+  //react 의 state
   data() {
     return {
+      isOpen: false,
+      count: [0, 0, 0],
+      menus: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  methods : {
+    increase(i) {
+      this.count[i]++
     }
   },
   components: {
@@ -44,5 +72,25 @@ export default {
 .menu a {
   color : white;
   padding : 10px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
