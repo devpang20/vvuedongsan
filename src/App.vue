916ac73f677ece1f3b8be70dcdb212d99de3abac
sik2 <devpang20@gmail.com>
2022-02-19 14:57:34 +0900

    feat: instagram clone setup

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,27 @@
 <template>
   <div id="app">
-    <Modal :oneRooms="oneRooms" :clickIndex="clickIndex" :isOpen="isOpen" />
-    <div class="menu">
-      <a v-for="(a, i) in menus" :key="i" >{{ a }}</a>
-    </div>
-    <Discount/>
-    <Card @openModal="isOpen = true; clickIndex = $event" v-for="(room, i) in oneRooms" :key="i" :room="oneRooms[i]" :clickIndex="clickIndex" :isOpen="isOpen" />
+    vvueStagram
   </div>
 </template>
 
 <script>
 
-import data from './assets/oneroom'
-import Discount from './components/Discount'
-import Modal from './components/Modal'
-import Card from './components/Card'
-
 export default {
   name: 'App',
-  //react 의 state
   data() {
     return {
-      oneRooms :data,
-      clickIndex : 0,
-      isOpen: false,
-      count: [0, 0, 0],
-      menus: ['Home', 'Shop', 'About'],
-      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
   },
   methods : {
-    increase(i) {
-      this.count[i]++
-    }
+
   },
   components: {
-    Discount,
-    Modal,
-    Card
+
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.menu {
-  background : darkslateblue;
-  padding : 15px;
-  border-radius : 5px;
-}
-.menu a {
-  color : white;
-  padding : 10px;
-}
-.room-img {
-  width: 100%;
-  margin-top: 40px;
-}
-body {
-  margin : 0;
-}
-div {
-  box-sizing: border-box;
-}
-.black-bg {
-  width: 100%; height:100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
-}
-.white-bg {
-  width: 100%; background: white;
-  border-radius: 8px;
-  padding: 20px;
 }
 </style>
