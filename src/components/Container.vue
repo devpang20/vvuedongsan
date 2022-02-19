0208189f69e28cd41ecd6d1e0056b697079936a1
sik2 <devpang20@gmail.com>
2022-02-19 15:15:27 +0900

    feat: vuestagram 컴포넌트 추가

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>
+</template>
+
+<script>
+import Post from './Post'
+
+export default {
+  name: 'container',
+  components: {
+    Post
+  }
+}
+
+</script>
+
+<style>
+
+</style>
